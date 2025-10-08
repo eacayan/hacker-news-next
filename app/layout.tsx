72eb0aedfa67ec917be0c337e0bc8347b026979a
Ellice Acayan <acayanellice@gmail.com>
2025-10-08 21:37:29 +0800

    feature: add homepage link to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import styles from './layout.module.scss';
 import './globals.scss';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Hacker News Explorer',
@@ -13,7 +14,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <header className={styles.header}>
           <div className={styles.container}>
-            <h1>Hacker News Explorer</h1>
+            <Link href="/">
+              <h1>Hacker News Explorer</h1>
+            </Link>
           </div>
         </header>
         <main className={styles.container}>{children}</main>
